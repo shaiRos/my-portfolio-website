18c32b2a6919883dd6d6df96a2bf5de00c64526a
Shaina <shainamae98@gmail.com>
2024-03-07 19:08:06 -0700

    added profile pic

--- src/sections/LandingIntro/LandingIntro.tsx
+++ src/sections/LandingIntro/LandingIntro.tsx
@@ -1,14 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import holder from '../../assets/profile.jpg'
 
 export default function LandingIntro() {
 
     return (
         <div className="flex justify-center mt-40 my-16 h-[50vh]">
-            <div className="h-[600px] w-[800px]" style={{backgroundColor: "#ECE7E1"}}>
+            <div className="h-[600px] w-[800px]" style={{ backgroundColor: "#ECE7E1" }}>
             </div>
-            <div className="absolute h-[600px] flex gap-40 items-center">
+            <div className="absolute h-[600px] flex gap-32 items-center">
                 <div className="flex flex-col gap-8 max-w-[500px]">
                     <label className="text-6xl">Full Stack Software Developer</label>
                     <section className="text-xl">Lorem Ipsum is simply has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</section>
@@ -20,7 +21,14 @@ export default function LandingIntro() {
                     </div>
                 </div>
                 <div>
-                    <div className="h-[250px] w-[250px] bg-red-300"></div>
+                    <img
+                        width={400}
+                        height={400}
+                        src={holder}
+                        className="img-fluid rounded-full hoverable shadow-sm"
+                        alt="Self Portrait"
+                    />
+                    {/* <div className="h-[250px] w-[250px] bg-red-300"></div> */}
                 </div>
             </div>
         </div>

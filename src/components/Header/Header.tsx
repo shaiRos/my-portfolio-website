1bf6835d6739aef530d7826875c077601da65f0a
Shaina <shainamae98@gmail.com>
2024-12-11 09:57:59 -0700

    header order change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,10 +8,10 @@ export default function Header({ scrollRefs }) {
                 <button className="text-4xl" style={{ fontWeight: 500 }} onClick={() => window.scrollTo(0, 0)}><span className="font-bold">Shaina</span> Rosell</button>
                 <div className="hidden md:flex flex-row gap-20">
                     <button onClick={() => scrollToRef(scrollRefs.aboutRef)}>About</button>
-                    <button onClick={() => scrollToRef(scrollRefs.projectsRef)}>Portfolio</button>
                     <a className="flex items-center" href="/Resume.pdf" target="_blank">
                         <button>Resume</button>
                     </a>
+                    <button onClick={() => scrollToRef(scrollRefs.projectsRef)}>Portfolio</button>
                     <button onClick={() => scrollToRef(scrollRefs.contactRef)}>Contact</button>
                 </div>
 
